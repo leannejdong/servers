c8d7d3daecb1939a0010335ac9832624ec7fb44f
Leanne J Dong <jdleanne@gmail.com>
2022-09-16 00:14:06 +1000

    Resigning add routes
    Signed-off-by: Leanne J Dong <jdleanne@gmail.com>

--- htmlFile.js
+++ htmlFile.js
@@ -18,6 +18,15 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
